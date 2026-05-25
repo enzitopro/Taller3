45f850e17b69b3fba29bf7ac5d0ecc44ee6a6b3d
enzitopro <ajipicantemrdayt@gmail.com>
2026-05-25 15:40:25 -0400

    avance lectura de archivos

--- logica/SistemaImpl.java
+++ logica/SistemaImpl.java
@@ -2,7 +2,9 @@ package logica;
 
 import dominio.Hechizo;
 import dominio.Mago;
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class SistemaImpl implements ISistema {
 	private ArrayList<Hechizo> listaGlobalHechizos;
@@ -15,6 +17,22 @@ public class SistemaImpl implements ISistema {
 
 	@Override
 	public void cargarHechizos() {
+		try {
+			File hechizos = new File("Hechizos.txt");
+			Scanner lectorHechizos = new Scanner(hechizos);
+			while (lectorHechizos.hasNextLine()) {
+				String linea = lectorHechizos.nextLine();
+				String[] partes = linea.split(";");
+				String nombre = partes[0];
+				String tipo = partes[1];
+				int dano = Integer.parseInt(partes[2]);
+				
+				
+			}
+			
+		} catch (Exception e) {
+			System.out.println("Archivo no encontrado");
+		}
 		
 	}
 
