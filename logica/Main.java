f9b93eaea9498c42937b1c1bc93d5239e9e00313
enzo de rodt <ajipicantemrdayt@gmail.com>
2026-06-09 11:24:28 -0400

    add datos
    ups

--- logica/Main.java
+++ logica/Main.java
@@ -1,5 +1,9 @@
 package logica;
 
+// Integrante 1: Elliot Bravo de Rodt - enzitopro
+// Integrante 2: Enzo Salvatore Cornieles Medina - justamago
+// Link repositorio: https://github.com/enzitopro/Taller3
+
 import java.util.Scanner;
 
 public class Main {
